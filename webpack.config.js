9dfb4a0a1edba068f07f14c9785271a2a2ea571b
Max Dobler <mdblr@outlook.com>
2016-10-26 19:47:40 -0700

    initial config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,36 @@
-var path = require('path')
+var path = require('path');
+var html = require('html-webpack-plugin');
+var script = process.env.npm_lifecycle_event;
 
-module.exports = {
-  entry: './lib/app/app.module.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+module.exports = (function config() {
+  let config = {};
+
+  config.entry = './src/app/app.module.js';
+
+  config.output = {};
+  config.output.path = path.join(__dirname, 'dist');
+  config.output.filename = 'bundle.[hash].js';
+
+  config.devtools = script === 'build' ? 'source-map' : 'eval-source-map';
+
+  config.module = {};
+  config.module.loaders = [{
+    test: path.join(__dirname, 'src/app'),
+    loaders: ['ng-annotate', 'babel-loader']
   },
-  devtools: 'inline-source-map',
-  module: {
-    loaders: [{
-      test: path.join(__dirname, 'lib/app'),
-      loader: 'babel-loader'
-    }]
-  }
-}
+  {
+    test: path.join(__dirname, 'src/app'),
+    include: /\.html$/,
+    loader: 'ngtemplate?relativeTo=' + (path.resolve(__dirname, './src')) + '/!html'
+  }];
+
+  config.resolve.modulesDirectories = ['node_modules'];
+
+  config.plugins = [
+    new html({
+      template: 'src/index.html'
+    })
+  ];
+
+  return config;
+}());
