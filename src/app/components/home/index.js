a3f21a66bc7c7c9ec693c163e627d81166c7acb8
Max Dobler <mdblr@outlook.com>
2016-11-06 21:15:52 -0800

    rename directive with prefix

--- src/app/components/home/index.js
+++ src/app/components/home/index.js
@@ -1,10 +1,9 @@
-// import io from 'socket.io';
 import homeCtrl from './home.controller.js';
 import config from './config.js';
-import convo from '../../shared/convo/convo.directive';
+import convoDir from '../../shared/convo/convo.directive';
 
 export default angular
   .module('chat.home', [])
   .config(config)
   .controller('homeCtrl', homeCtrl)
-  .directive('convo', convo);
+  .directive('convoDir', convoDir);
