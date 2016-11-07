681cc2ee384ad88639e09f1a9e2cbe8f40142ddd
Max Dobler <mdblr@outlook.com>
2016-11-06 21:13:21 -0800

    add events for connecting, disconnecting, and message

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(9090);
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/src/index.html`);
 });
 
-
-http.listen(9090);
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  });
+  socket.on('message', (msg) => {
+    console.log(msg);
+    io.emit('message', msg); 
+  })
+});
