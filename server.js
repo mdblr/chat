382b308a268b443efbb86bb2688a42ac06952a4d
Max Dobler <mdblr@outlook.com>
2016-11-15 22:11:38 -0800

    add body-parser to server

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const chat = require('./socket.conf')(server);
+const bodyParser = require('body-parser');
+const users = require('./routes/users');
+const port = process.env.PORT || 9090;
 
-server.listen(9090);
+server.listen(port);
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/src/index.html`);
 });
+
+app.use('/users', users);
 
 chat();
