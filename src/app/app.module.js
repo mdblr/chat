aa9d6f75389c319712d87a1132d25b6561b35d5d
Max Dobler <mdblr@outlook.com>
2016-11-15 22:13:01 -0800

    add login module to app dependencies

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,9 +1,10 @@
 import angular from 'angular';
 import router from 'angular-route';
 import config from './app.routing.js';
-import './components/home/index.js';
+import './components/home';
+import './components/login';
 
 
 export default angular
-  .module('chat', ['ngRoute', 'chat.home'])
+  .module('chat', ['ngRoute', 'chat.login', 'chat.home'])
   .config(config);
