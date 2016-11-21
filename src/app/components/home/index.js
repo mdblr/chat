4278129269bb3ff667ed16eaa30f649003b5b9e9
Max Dobler <mdblr@outlook.com>
2016-11-20 22:17:20 -0800

    add user factory to module

--- src/app/components/home/index.js
+++ src/app/components/home/index.js
@@ -1,9 +1,11 @@
 import homeCtrl from './home.controller.js';
 import config from './config.js';
 import convoDir from '../../shared/convo/convo.directive';
+import user from '../../shared/user/user.factory';
 
 export default angular
   .module('chat.home', [])
   .config(config)
   .controller('homeCtrl', homeCtrl)
-  .directive('convoDir', convoDir);
+  .factory('user', user)
+  .directive('convoDir', convoDir)
--- src/app/components/login/index.js
+++ src/app/components/login/index.js
@@ -1,9 +1,10 @@
 import config from './config.js';
 import loginCtrl from './login.controller.js';
-import username from './username.directive.js'; 
+import username from './username.directive.js';
+import user from '../../shared/user/user.factory';
 
 export default angular
   .module('chat.login', [])
   .config(config)
-  .controller('loginCtrl',loginCtrl)
+  .factory('user', user)
   .directive('username', username);
