8529b676b48a4ab727b0077ddfe5957d1eb2b52d
Max Dobler <mdblr@outlook.com>
2016-11-20 22:16:21 -0800

    send more data in socket event, get current user from factory

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -2,20 +2,24 @@
 
 import io from 'socket.io-client';
 
-export default function home($scope) {
-  var socket = io.connect()
-
+export default function home($scope, user) {
+  const socket = io.connect()
   const vm = this;
+
+  vm.username = user.get();
   vm.message = '';
   vm.convo = [];
   vm.send = () => {
     if (!vm.message.trim()) return false;
-    socket.emit('message', vm.message);
+    socket.emit('message', {
+      msg: vm.message,
+      user: vm.username
+    });
     vm.message = '';
   }
 
-  socket.on('message', (msg) => {
-    vm.convo.push(msg);
+  socket.on('message', data => {
+    vm.convo.push(data);
     $scope.$apply();
   });
 }
