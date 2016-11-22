71c711c77be843632e2ebddb42f1ee899e4a007d
Max Dobler <mdblr@outlook.com>
2016-11-21 19:40:26 -0800

    add message factory to angular module

--- src/app/components/home/index.js
+++ src/app/components/home/index.js
@@ -2,10 +2,12 @@ import homeCtrl from './home.controller.js';
 import config from './config.js';
 import convoDir from '../../shared/convo/convo.directive';
 import user from '../../shared/user/user.factory';
+import message from '../../shared/convo/message.factory';
 
 export default angular
   .module('chat.home', [])
   .config(config)
   .controller('homeCtrl', homeCtrl)
   .factory('user', user)
+  .factory('message', message)
   .directive('convoDir', convoDir)
