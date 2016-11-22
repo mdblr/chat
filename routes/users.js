ca4c5158c40f29eaf3295c8fe18cac345e498366
Max Dobler <mdblr@outlook.com>
2016-11-21 19:36:47 -0800

    implement delete endpoint for removing user from currentUsers

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-var usernames = [];
+var currentUsers = [];
 
 router.post('/current', (req, res) => {
-  let userExists = usernames.indexOf(req.body.username) < 0 ? false : true;
+  let userExists = currentUsers.indexOf(req.body.username) < 0 ? false : true;
   res.send({userExists});
 });
 
 router.post('/add', (req, res) => {
-  usernames.push(req.body.username);
+  currentUsers.push(req.body.username);
   res.end();
 });
+
+router.delete('/remove', (req, res) => {
+  let index = currentUsers.indexOf(req.body.username);
+  if (index < 0) return false;
+  currentUsers[index] = currentUsers[currentUsers.length - 1];
+  currentUsers.length -= 1;
+})
 
 module.exports = router;
