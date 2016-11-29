7e05503bcdce09edf7e75a8b4911e0896ca5a5e7
Max Dobler <mdblr@outlook.com>
2016-11-29 15:39:52 -0800

    put editUser onto view model

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -9,11 +9,22 @@ export default function home(user, message, $scope, $window) {
   const vm = this;
 
   vm.username = user.get();
+  vm.newName;
   vm.userMessage;
   vm.conversation = [];
-  vm.send = () => {
-    message.send(socket, vm.userMessage, vm.username);
+  vm.send = (userMessage, username) => {
+    message.send(socket, userMessage, username);
     vm.userMessage = '';
+  };
+  vm.editUser = (username, newName) => {
+    let temp = username;
+    user.editUser(username, newName)
+      .then( res => {
+        user.set(res.data.user);
+        vm.username = user.get();
+        message.userChange(socket, temp, newName);
+        vm.newName = ''; 
+      });
   };
   message.receive(socket, vm.conversation, $scope);
   $window.onbeforeunload = username => {
